cd9ef7ab5525c37085ccfb5b4bab031d3547b8f0
cm2404 <mayhewcharlie24@gmail.com>
2021-11-25 14:15:29 +0000

    added grid, added food

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import pygame, sys
+from pygame.math import Vector2
+
+class FOOD:
+    def __init__(self):
+        self.x = 5
+        self.y = 4
+        self.pos = pygame.Vector2(self.x, self.y)
+
+    def draw_food(self):
+        food_rect = pygame.Rect(self.pos.x *cell_size, self.pos.y *cell_size, cell_size, cell_size)
+        pygame.draw.rect(screen,(255,0,0),food_rect)
 
 # setting up display
 back_colour = (202,234,3)
-screen = pygame.display.set_mode((300,300))
+
+cell_size = 30 # length and height of cell in pixels
+cell_number = 20 # number of cells on each row
+screen = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_number))
 pygame.display.set_caption('Snek')
 
 screen.fill(back_colour)
@@ -11,12 +25,15 @@ pygame.display.flip()
 # clock
 clock = pygame.time.Clock()
 
+food = FOOD()
+
 #game loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    
     pygame.display.update()
     clock.tick(60)
 
