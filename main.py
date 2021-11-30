f933b75a1a88cdb9ecce3c3310f582b64f192a25
cm2404 <mayhewcharlie24@gmail.com>
2021-11-30 16:17:01 +0000

    added apple

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ class FOOD:
     def __init__(self):
         self.x = 5
         self.y = 4
-        self.pos = pygame.Vector2(self.x, self.y)
+        self.pos = Vector2(self.x, self.y)
 
     def draw_food(self):
-        food_rect = pygame.Rect(self.pos.x *cell_size, self.pos.y *cell_size, cell_size, cell_size)
+        food_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size, cell_size, cell_size)
         pygame.draw.rect(screen,(255,0,0),food_rect)
 
 # setting up display
@@ -16,7 +16,7 @@ back_colour = (202,234,3)
 
 cell_size = 30 # length and height of cell in pixels
 cell_number = 20 # number of cells on each row
-screen = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_number))
+screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
 pygame.display.set_caption('Snek')
 
 screen.fill(back_colour)
@@ -35,5 +35,6 @@ while True:
             sys.exit()
     
     pygame.display.update()
+    food.draw_food()
     clock.tick(60)
 
